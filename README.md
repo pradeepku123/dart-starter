ae28148cf8cebdbbac32e217a5afe3be0c5ce29a
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-05-08 11:48:32 +0530

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+## Learn Dart programming language
---
+++ bin/main.dart
@@ -0,0 +1,8 @@
+/**
+ * This is the main entry point for the application.
+ */
+void main() {
+  print("Hello World");
+  //variable Names & Types
+}
+// End of generated code.
